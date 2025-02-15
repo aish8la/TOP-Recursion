01e578056b09ed85126ab664e8e65ee32e278e2a
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-02-15 18:41:29 +0500

    Add fibs iterative function

---
+++ fibonacci-loop.js
@@ -0,0 +1,14 @@
+function fibs(num) {
+
+    if(num < 2) return;
+
+    let fibArray = [0, 1];
+
+    for(let i = 2; i < num; i++) {
+        fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    }
+
+    return fibArray
+}
+
+console.log(fibs(8));
